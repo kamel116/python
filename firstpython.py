8ec14a55f7d42bfd1966031d30635975ec817fb8
kamel116 <kamel116@hotmail.com>
2020-05-19 02:20:53 +0300

    hello world multi line new line

--- firstpython.py
+++ firstpython.py
@@ -1,1 +1,7 @@
-print("first")
+# this is a hello world applications
+print("hello world! \n")
+
+print('hello world 2')
+# multi line
+print('''this is multi line hello world today 
+is a very sunny''')
