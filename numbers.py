04e8f05415be2f5c9ea5a02e8112b10b314e9335
kamel116 <kamel116@hotmail.com>
2020-05-20 02:10:51 +0300

    using operators in math

---
+++ numbers.py
@@ -0,0 +1,20 @@
+print(1 + 1)
+print('10' + '20')
+# error result in this code canot concatenate numbers with strings
+#print(10 + '20')
+# CASTING convert string to number
+print(10 + int('20'))
+#using operators in math
+print('using operators in math:')
+print(10 + 10)
+print(20 - 15)
+print(20 * 2)
+print(12/3)
+print('power of')
+print(3 ** 10)
+num1 = 10.1
+num2 = 20
+num3 = num1 + num2
+print(num3)
+print(type(num3))
+#num2 += 1  ====> num2 = num2 + 1
