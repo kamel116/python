2fa8b9a6faafffe17ecdcabc4b37e31f5fd6f506
kamel116 <kamel116@hotmail.com>
2020-05-22 01:48:19 +0300

    logic operator commit

---
+++ operators.py
@@ -0,0 +1,34 @@
+num1 = 5
+num2 = 6
+print(num1 > num2)
+#this result is False
+print(num1 < num2)
+#this result is True
+print(5 > 6)
+print(5 < 6)
+print(5 == 6)
+print(5 == 5)
+print(5 != 6)
+print(5 != 5)
+print(5 >= 6)
+print(5 <= 6)
+#more conditions
+print(True and True) #True
+print(True and False) # False
+print(False and False) #False
+print(True or True) #True
+print(True or False) # True
+print(False or False) # False
+print(not True) #False
+print(not False) #True
+a,b=1,2
+print(a)
+print(b)
+print(a==a and b==b) #True
+print(a==a and b<a) #False
+print(a>b and b<a) #False
+print(a==a or b==b)#True
+print(a==a or b<a)#True
+print(a>b or b<a) #False
+print(not (a != 5))#False
+print(not (a == 5)) #true
