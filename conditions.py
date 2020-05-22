96f580ab954aaa55f6be4fc15feec5387badd26f
kamel116 <kamel116@hotmail.com>
2020-05-23 02:04:42 +0300

    condition commit

---
+++ conditions.py
@@ -0,0 +1,6 @@
+username = input('please insert username: ')
+password = input('please insert password: ')
+if username == 'kamel' and password == '123':
+    print('welcome to ur email')
+else:
+    print('Error')    
