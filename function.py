d55ceabcc15761aee3bad3cd2fd38a978295a7cf
kamel116 <kamel116@hotmail.com>
2020-05-26 10:53:16 +0300

    function

---
+++ function.py
@@ -0,0 +1,14 @@
+print("my name is kamel")
+print(22)
+def linear():
+    print('-------------------')
+print('kamel')
+linear()
+def addition(num1,num2):
+    print(int(num1) + int(num2))
+
+n1 = input('enter first num: ')
+n2 = input('enter second num: ')
+addition(n1,n2)
+
+
