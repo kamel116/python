c011a68c76977c3db737cb1eaa7f4f8e602881c2
kamel116 <kamel116@hotmail.com>
2020-05-26 10:26:05 +0300

    dictionaries commit

---
+++ dictionaries.py
@@ -0,0 +1,8 @@
+subjects_with_num_students = {'Arabic':3 ,'English':3 ,'Math':2}
+print(subjects_with_num_students)
+students = {'Arabic':['Ali','Mohammad','Mona'] , 'English':['jack','Ahmad','Moneef']}
+print(students)
+students['Math'] = ['Slah','Husain']
+print(students)
+students.update({'History':'fadi'})
+print(students)
