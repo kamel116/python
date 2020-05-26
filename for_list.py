b50ec2bfcde7b1329e52820f1bcfd50fcdf6d6b8
kamel116 <kamel116@hotmail.com>
2020-05-26 10:01:30 +0300

    for loop and list

---
+++ for_list.py
@@ -0,0 +1,18 @@
+subjects = ['Arabics','English','Math']
+print(subjects)
+print(subjects[0])
+print(subjects[1])
+print(subjects[2])
+subjects.append('History')
+print(subjects)
+print(subjects[1:3])
+print(subjects[1:4])
+print(subjects[-1])
+#subjects.pop()
+#print(subjects)
+#subjects.pop(0)
+#print(subjects)
+
+
+for x in subjects:
+    print(x)
